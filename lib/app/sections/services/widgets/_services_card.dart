9dfabc5283a736c882bb4d041c1319b716f2cd54
saliemmanuel <saliemmanuel3304@gmail.com>
2023-08-22 03:22:24 +0100

    initial commit

---
+++ lib/app/sections/services/widgets/_services_card.dart
@@ -0,0 +1,93 @@
+part of '../services.dart';
+
+class _ServiceCard extends StatefulWidget {
+  final ServicesUtils service;
+
+  const _ServiceCard({Key? key, required this.service}) : super(key: key);
+
+  @override
+  _ServiceCardState createState() => _ServiceCardState();
+}
+
+class _ServiceCardState extends State<_ServiceCard> {
+  // bool isHover = false;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return InkWell(
+      hoverColor: Colors.transparent,
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+      onTap: () {},
+      child: Container(
+        width: Responsive.isTablet(context) ? 400 : 300,
+        // height: AppDimensions.normalize(100),
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+        decoration: BoxDecoration(
+          // gradient: isHover ? pinkpurple : grayBack,
+          gradient: theme.serviceCard,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [blackColorShadow],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              widget.service.icon,
+              height: 60,
+            ),
+            Space.y(3.w)!,
+            Text(widget.service.name,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: theme.textColor,
+                )),
+            Space.y(1.w)!,
+            Text(
+              widget.service.description,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: theme.textColor,
+                fontWeight: FontWeight.w200,
+                fontSize: 13,
+              ),
+            ),
+            Space.y(2.w)!,
+            if (Responsive.isDesktop(context))
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: widget.service.tool
+                      .map((e) => Row(
+                            children: [
+                              const Text('🛠   '),
+                              Text(e,
+                                  style: TextStyle(
+                                    color: theme.textColor,
+                                  )),
+                            ],
+                          ))
+                      .toList()),
+            if (Responsive.isMobile(context) || Responsive.isTablet(context))
+              Expanded(
+                child: ListView(
+                    padding: EdgeInsets.zero,
+                    shrinkWrap: true,
+                    children: widget.service.tool
+                        .map((e) => Row(
+                              children: [
+                                const Text('🛠   '),
+                                Text(e,
+                                    style: TextStyle(
+                                      color: theme.textColor,
+                                    )),
+                              ],
+                            ))
+                        .toList()),
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/changes/links.dart
@@ -0,0 +1,7 @@
+// CV
+const String resume = 'https://drive.google.com/file/d/1rCTjCEyYGqaWInZYquBmWbguqbMd9617/view';
+const String gitHub = 'https://github.com/saliemmanuel';
+const String facebook = 'https://www.facebook.com/jonhsone.emmanuel';
+const String youtube = 'https://www.youtube.com/@saliemmanueldotdev3896';
+const String instagram = 'https://www.instagram.com/_dotdev/';
+const String whatsapp = 'https://api.whatsapp.com/send?phone=237698066896';
